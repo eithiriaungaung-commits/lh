d80b7d53d721e877c13ad9011b297d7441c33510
Lay Hlaing <eithiriaung.aung@gmail.com>
2026-04-16 18:57:30 +0630

    add webhook route

--- app.py
+++ app.py
@@ -6,24 +6,15 @@ app = Flask(__name__)
 BOT_TOKEN = "8694124474:AAFSPgMvgRlOKFiWC8ARdB1jnb8tMG4hkkQ"
 
 USER_MAP = {
-    "layhlaingeithiriaung": "你的chat_id"
+    "69ba04dd6dfc6f22c864f8f9": "你的telegram_chat_id"
 }
 
-def send_telegram(chat_id, text):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    response = requests.post(url, json={
-        "chat_id": chat_id,
-        "text": text
-    })
+@app.route("/", methods=["GET"])
+def home():
+    return "Webhook running"
 
-    print(response.status_code)
-    print(response.text)
-
-@app.route("/trello-webhook", methods=["HEAD", "POST"])
+@app.route("/webhook", methods=["POST"])
 def webhook():
-    if request.method == "HEAD":
-        return "", 200
-
     data = request.json
     print(data)
 
@@ -31,19 +22,21 @@ def webhook():
     action_type = action.get("type")
 
     if action_type == "addMemberToCard":
-        member = action["member"]["username"]
+        member_id = action["data"]["idMember"]
         card_name = action["data"]["card"]["name"]
 
-        chat_id = USER_MAP.get(member)
+        chat_id = USER_MAP.get(member_id)
 
         if chat_id:
-            send_telegram(
-                chat_id,
-                f"📌 新任务已分配：{card_name}"
+            requests.post(
+                f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+                json={
+                    "chat_id": chat_id,
+                    "text": f"你有新的 Trello 任务：{card_name}"
+                }
             )
 
-    return "OK", 200
-
+    return "ok", 200
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=10000)
